cc53df58e36501fdd2647b40c1cd3484e7cb7218
Steve Toosevich <stoosevich@gmail.com>
2014-04-01 14:32:39 -0500

    first commit

---
+++ RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  Lost
+//
+//  Created by Steve Toosevich on 4/1/14.
+//  Copyright (c) 2014 Steve Toosevich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController
+
+@end
